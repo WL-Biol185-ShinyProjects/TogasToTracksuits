e776e34286fa3e7a488984c23c3bbc08d4f952ce
Jack Roszko <jroszko@mail.wlu.edu>
2026-03-09 15:53:19 -0400

    '

---
+++ server.R
@@ -0,0 +1,21 @@
+
+#loading data
+Olympic_Dataset = read.csv("athlete_events-Olympic Dataset.csv")
+
+#loading the appropriate libraries
+library(wordcloud)
+library(tm)
+library(memoise)
+library(tidyverse)
+library(stringr)
+library(ggplot2)
+library(readxl)
+library(shiny)
+library(leaflet)
+library(leaflet.extras)
+library(dplyr)
+library(sf)
+library(lubridate)
+library(maps)
+library(gridExtra)
+
